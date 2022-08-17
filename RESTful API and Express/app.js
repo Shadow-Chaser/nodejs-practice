c97feccce00f9d633265fb02c6aa086bc07f035c
Shadow-Chaser <ashabhtanim@gmail.com>
2022-08-17 20:33:28 +0600

    [Fixed] my-app-extended app.js

--- RESTful API and Express/app.js
+++ RESTful API and Express/app.js
@@ -14,9 +14,7 @@ app.get('/', (request, response) => {
 
 app.get('/api/students', (req, res) => {
     db.getDbStudents()
-        .then(students => {
-            res.send(students);
-        });
+        .then(students => res.send(students));
 });
 
 app.post('/api/students', (req, res) => {
@@ -28,6 +26,32 @@ app.post('/api/students', (req, res) => {
                 .then(data => {
                     res.send(student);
                 });
+        });
+});
+
+app.get('/api/students/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    db.getDbStudents()
+        .then(students => {
+            const student = students.find(s => s.id === id);
+            if (!student) res.status(404).send("No student found with this id!");
+            else res.send(student);
+        });
+});
+
+app.put('/api/students/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const updatedData = req.body;
+    db.getDbStudents()
+        .then(students => {
+            const student = students.find(s => s.id === id);
+            if (!student) res.status(404).send("No student found with this id!");
+            else {
+                const i = students.findIndex(s => s.id === id);
+                students[i] = updatedData;
+                db.insertDbStudent(students)
+                    .then(msg => res.send(updatedData));
+            }
         });
 });
 
