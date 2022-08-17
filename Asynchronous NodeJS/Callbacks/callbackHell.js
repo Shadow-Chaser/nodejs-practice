611b136f3891118cdbe96dc634f8064b87953d65
Shadow-Chaser <ashabhtanim@gmail.com>
2022-08-17 15:54:04 +0600

    [Added] promises

--- Asynchronous NodeJS/Callbacks/callbackHell.js
+++ Asynchronous NodeJS/Callbacks/callbackHell.js
@@ -1,4 +1,10 @@
-// Callback Function
+/*
+Callback Hell: Callback Hell is essentially nested callbacks 
+stacked below one another forming a pyramid structure. Every 
+callback depends/waits for the previous callback, thereby 
+making a pyramid structure that affects the readability 
+and maintainability of the code. 
+*/
 console.log("Line 1");
 
 getStudent(2, (student) => {
