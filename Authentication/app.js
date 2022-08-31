d0801092c46f220e78da459febaa0742db10b88e
Shadow-Chaser <ashabhtanim@gmail.com>
2022-08-31 20:40:35 +0600

    [Added] authentication

---
+++ Authentication/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const userRouter = require('./routers/userRouter');
+const authRouter = require('./routers/authRouter');
+const morgan = require('morgan');
+
+app.use(express.json());
+
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
+
+
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
+
+app.get('/', (req, res) => {
+    res.send("Hello from express js!");
+});
+
+module.exports = app;
